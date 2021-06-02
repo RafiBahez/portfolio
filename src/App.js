8590e7889c03c0c7986919942a4285766b49e641
Mohammad Rafi Bahez <rafibahez@gmail.com>
2021-06-02 14:16:45 -0700

    Add Basic Banner View

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import "./App.css";
 
-import Navigation from './components/Navigation';
+import Navigation from "./components/Navigation";
+import Banner from "./views/Banner";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
+      <Navigation />
 
-    <Navigation transparent="true"/>
-      
+      <div className="grid grid-flow-col">
+        <div className="w-20 bg-black">left side</div>
+
+        <div className="grid-cols-10">
+          <Banner />
+        </div>
+        <div className="w-20 bg-black">right side</div>
+      </div>
     </div>
   );
 }
---
+++ src/views/Banner.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Banner extends Component {
+  render() {
+    return (
+        <div className="bg-port-black h-full px-20 py-20">
+           
+           <h3 className="text-white pb-5">Hi, my name is </h3>
+           <h1 className="text-white text-5xl">Mohammad Rafi Bahez</h1>
+           <h2 className="text-gray-300 text-3xl">I'm Software Engineer & Full Stack Web Developer</h2>
+           <p className="text-white text-base w-8/12 pt-5 pb-14"> I’m software engineer who specializes in building creative digital experiences. I learned and worked with many technologies over the past years, and now I love working with JavaScript and PHP.</p>
+
+           <a className="text-white border border-port-orange px-3 py-2">Get in Touch</a>
+        </div>
+    );
+  }
+}
+
+export default Banner;
