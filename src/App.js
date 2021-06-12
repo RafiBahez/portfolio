ed63ce176fdb713c7a1d0f258ee301a08ad30293
Mohammad Rafi Bahez <rafibahez@gmail.com>
2021-06-11 21:26:50 -0700

    Add SocialMeida Icons for LeftSide

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import About from "./views/About";
 import Experince from "./views/Experience";
 import Contact from "./views/Contact";
 
+// leftside social media links
+import SocialMedia from "./views/SocialMedia";
+
 function App() {
   return (
     <div className="App ">
       <Navigation />
 
       <div className="grid grid-flow-col">
-        <div className="w-20 bg-port-main">left side</div>
+        <div className="w-16 h-full bg-main-darker">
+            <SocialMedia />
+        </div>
 
         <div className="grid-cols-10">
           <Banner />
