5bd4d917db204bfaa5084490468e38c6be361a08
Mohammad Rafi Bahez <rafibahez@gmail.com>
2021-09-17 12:27:15 -0700

    Updated Banner

--- src/views/Banner.js
+++ src/views/Banner.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import {Link} from "react-scroll";
+
 class Banner extends Component {
   render() {
     let style = { height: "calc(100vh - 82px)" };
@@ -8,24 +10,17 @@ class Banner extends Component {
         <h3 className="text-white pb-5">Hi, my name is </h3>
         <h1 className="text-white text-5xl">Mohammad Rafi Bahez</h1>
         <h2 className="text-port-gray text-3xl">
-          I'm Software Engineer & Full Stack Web Developer
+          I'm a Software Engineer 
         </h2>
         <p className="text-white text-base w-8/12 pt-5 pb-14">
           {" "}
           I’m software engineer who specializes in building creative digital
           experiences. I learned and worked with many technologies over the past
-          years, and now I love working with JavaScript and PHP.
+          years.
         </p>
 
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            window.location.href = "#contact";
-          }}
-          className="text-white border border-port-second cursor-pointer px-3 py-2 transition duration-500 ease-in-out transform hover:bg-port-second hover:text-port-main hover:border-main-darker"
-        >
-          Get in Touch
-        </button>
+        <Link className="text-white border border-port-second cursor-pointer px-3 py-3 transition duration-500 ease-in-out transform hover:bg-port-second hover:text-port-main hover:border-main-darker" activeClass="active" to="contact"  spy={true} smooth={true} offset={50} duration={500}>Get in Touch</Link>
+
       </div>
     );
   }
