b23febad32f285aea819075fce5decbc0886f49b
Mohammad Rafi Bahez <rafibahez@gmail.com>
2021-09-23 11:06:45 -0700

    Fixes lines in  Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ class Title extends Component {
     return (
       <div className="flex flex-row items-center gap-3 pt-10 pb-14">
         <div
-          className={"hidden sm:block bg-second-darker h-1 w-32 " + this.props.showLeftLine}
+          className={"hidden sm:block bg-second-darker h-1 w-32 sm:" +this.props.showLeftLine}
         ></div>
         <div>
           <h1 className="text-white text-3xl pb-5"> {this.props.children} </h1>
